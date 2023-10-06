bf09fa610f163dbe0bafc300a71aad454f6222ac
CelioDiazLezama27 <142622900+CelioDiazLezama27@users.noreply.github.com>
2023-10-06 14:06:09 -0500

    Add files via upload

---
+++ src/t2_tpoo/T2_TPOO.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package t2_tpoo;
+
+/**
+ *
+ * @author Celio D
+ */
+public class T2_TPOO {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
